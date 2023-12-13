7dff64e3e08472214766c265f7713b35dd5c9fab
patboy04 <theofficialjp@gmail.com>
2023-12-13 17:19:19 +0800

    Added home page with fetch req to render chars

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom"
+import Layout from "./components/Layout.jsx"
+import Home, {loader as homeLoader }from "./pages/Home.jsx"
+import Character from "./pages/Character.jsx"
+import Error from "./pages/Error.jsx"
 
 const App = () => {
+    const router = createBrowserRouter(createRoutesFromElements(
+        <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} loader={homeLoader}/>
+            <Route path=':character' element={<Character />}/>
+        </Route>
+    ))
+
   return (
-    <div>
-        <h1 className='text-5xl'>App</h1>
-    </div>
+    <RouterProvider router={router}/>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+export const loader = async() => {
+    const res = await fetch('https://api.genshin.dev/characters')
+    if(!res.ok) {
+        throw new Error(("Failed to load characters"))
+    }
+    const data = await res.json()
+    return data
+}
+const Home = () => {
+
+    const data = useLoaderData()
+
+    const renderCharacters = data.map(character => {
+        //string manipulations for chars with hyphen
+        let modChar = character.replace("-", "_")
+        //string manipulations for chars with lastname, firstname convention
+        switch(modChar) {
+            case 'ayaka':
+                modChar = "kamisato_ayaka";
+                break;
+            case 'ayato':
+                modChar = "kamisato_ayato";
+                break;
+            case 'kazuha':
+                modChar = "kaedehara_kazuha";
+                break;
+            case 'kokomi':
+                modChar = "sangonomiya_kokomi";
+                break;
+            case 'raiden':
+                modChar = "raiden_shogun";
+                break;
+            case 'sara':
+                modChar = "kujou_sara";
+                break;
+        }
+        return (
+            <div className='from-[#52647a] bg-gradient-to-b flex flex-col shadow-2xl drop-shadow-2xl rounded-xl hover:scale-105 duration-500 cursor-pointer'>
+                <h1 className='text-[white] text-center bg-[#52647A]'>{modChar.replace("_", " ")}</h1>
+                <img src={`https://paimon.moe/images/characters/full/${modChar}.png`} className='object-cover w-full h-full'/>
+            </div>
+        )
+    })
+
+  return (
+    <div className='w-full bg-[#EDE5D8]'>
+        <div className='grid grid-cols-2 p-4 m-auto gap-10'>
+            {renderCharacters}
+        </div>
+    </div>
+  )
+}
+
+export default Home
