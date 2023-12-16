12be6346f34e2bdf3f556904b05a078c21eedb7a
patboy04 <theofficialjp@gmail.com>
2023-12-16 14:01:25 +0800

    fixed bug on active styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,28 +3,22 @@ import { NavLink, Outlet } from 'react-router-dom'
 
 const Navbar = () => {
     const activeLink = {
-        fontWeight: "bold",
-        textDecoration: "underline",
-        color: "#161616"
+        fontSize: "2.2rem"
+
+
     }
 
   return (
     <div className='w-full h-full bg-[#EDE5D8]'>
         <div className='flex justify-center items-end p-4 md:p-8 gap-8'>
-            <NavLink to='artifacts' style={({isActive})=> isActive ? activeLink : null}>
-                <h1 className='text-lg sm:text-xl xl:text-2xl text-center text-[#4B5366] '>
-                    Artifacts
-                </h1>
+            <NavLink to='artifacts' className='text-lg sm:text-xl xl:text-2xl text-center text-[#4B5366] ' style={({isActive})=> isActive ? activeLink : null}>
+                Artifacts
             </NavLink>
-            <NavLink to='.' style={({isActive})=> isActive ? activeLink : null} end>
-                <h1 className='text-3xl sm:text-4xl xl:text-5xl text-center text-[#4B5366] '>
-                    Characters
-                </h1>
+            <NavLink to='.' className='text-lg sm:text-xl xl:text-2xl text-center text-[#4B5366] ' style={({isActive})=> isActive ? activeLink : null} end>
+                Characters
             </NavLink>
-            <NavLink to='artifacts' style={({isActive})=> isActive ? activeLink : null}>
-                <h1 className='text-lg sm:text-xl xl:text-2xl text-center text-[#4B5366] '>
-                    Weapons
-                </h1>
+            <NavLink to='artifacts'  className='text-lg sm:text-xl xl:text-2xl text-center text-[#4B5366] 'style={({isActive})=> isActive ? activeLink : null}>
+                Weapons
             </NavLink>
         </div>
         <Outlet />
